ff6140032954ed5b50528c344fb8fe0e3fcf2b7d
patoag <pantinao.guairao@gmail.com>
2025-02-04 00:43:17 -0300

    implementacion inicial de docker y configuraciones necesarias para docker-compose

--- backend_django/backend_django/settings.py
+++ backend_django/backend_django/settings.py
@@ -97,8 +97,12 @@ WSGI_APPLICATION = 'backend_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', 5432),
     }
 }
 
